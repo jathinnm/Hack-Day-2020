8613675e0efa3b19da53713046d0a0bd52138399
OfficialPledge <evanhquist@gmail.com>
2020-07-15 11:07:56 -0500

    Added Areas to SE+CE

--- src/comp/core/CE.js
+++ src/comp/core/CE.js
@@ -6,32 +6,69 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Header from '../generic/Header.js'
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
 
 const drawerWidth = 100;
 const useStyles = makeStyles((theme) => ({
     root: {
       display: "flex"
     },
-
+    button:{
+      borderRadius: "10px",
+      backgroundColor: "#F01716",
+      width: "60%",
+      marginLeft: '10vh'
+  },
+  subtitle:{
+    primarys: '#FFFFF'
+}
   }));
 
-//   window.addEventListener("scroll", noscroll);
-//   function noscroll(){
-//       window.scrollTo(0,0);
-//   }
+  //   window.addEventListener("scroll", noscroll);
+  //   function noscroll(){
+  //       window.scrollTo(0,0);
+  //   }
+  const areas = ['Digital Experience', 'P&C (Auto & Fire) Bloomington','P&C (Auto & Fire) Dallas','P&C Claims','Life/Health/IPS, Bank','Agency/Marketing & Contact Centers','Digital Assets & Strategy','General Departments','Billing & Payments']
+  const leaders = ['Vijayasri Yerraguntla','Curt Stewart','Scott E. Taubert','Ritesh Saraf','Sara Donahue','Jason Potts','Mahesh Chandrappa','Dar Hakimi','Scott E. Taubert']
+  const teams = []
+  var tmparr =[]
+  export default function(){
+      const classes = useStyles();
+      return(
+        <div className={classes.root}>
+          <Grid container spacing ={10} direction = "column">
+              <Grid item>
+                  <Header title = "Customer Experience Division"/>
+               </Grid>
+               <Grid item container spacing={10} justify="center">
 
-export default function(){
-    const classes = useStyles();
-    return(
-      <div className={classes.root}>
-        <Grid container spacing ={10} direction = "column">
-            <Grid item>
-                <Header  title = "Customer Experience Division"/>
-             </Grid>
-            <Grid item>
-                <h1>CE Page</h1>
-            </Grid>
-        </Grid>
-        </div>
-    );
-}
+                  {areas.map(function (area,i) {
+                    tmparr = [area, leaders[i], teams[i]]
+                    if(tmparr != null){
+      return (
+                      <Grid item xs={6} alignItems = 'center'>
+                        <Button variant="contained"  className = {classes.button}  >
+                            <Grid container item alignItems='center' direction = 'column'>
+                              <Grid item>
+                                <Typography variant ='h5'>{tmparr[0]}</Typography>
+                              </Grid>
+                              <Grid item>
+                              <Typography className={classes.subtitle} >
+                                  {tmparr[1]}
+                              </Typography>
+                              </Grid>
+
+                            </Grid>
+                          </Button>
+                      </Grid>
+                  )
+                }})
+
+  }
+              </Grid>
+          </Grid>
+
+          </div>
+      );
+  }
--- src/comp/core/SE.js
+++ src/comp/core/SE.js
@@ -7,57 +7,68 @@ import Typography from '@material-ui/core/Typography';
 import Header from '../generic/Header.js'
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
 
 const drawerWidth = 100;
 const useStyles = makeStyles((theme) => ({
     root: {
       display: "flex"
     },
-    paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    },
-
+    button:{
+      borderRadius: "10px",
+      backgroundColor: "#F01716",
+      width: "60%",
+      marginLeft: '10vh'
+  },
+  subtitle:{
+    primarys: '#FFFFF'
+}
   }));
 
-//   window.addEventListener("scroll", noscroll);
-//   function noscroll(){
-//       window.scrollTo(0,0);
-//   }
+  //   window.addEventListener("scroll", noscroll);
+  //   function noscroll(){
+  //       window.scrollTo(0,0);
+  //   }
+  const areas = ['IT Risk and Compliance', 'Planning & Management']
+  const leaders = ['Brett Weber','Randy McBeath']
+  const teams = []
+  var tmparr =[]
+  export default function(){
+      const classes = useStyles();
+      return(
+        <div className={classes.root}>
+          <Grid container spacing ={10} direction = "column">
+              <Grid item>
+                  <Header title = "Strategic Engagement Division"/>
+               </Grid>
+               <Grid item container spacing={10} justify="center">
 
-export default function(){
-    const classes = useStyles();
-    return(
-      <div className={classes.root}>
-      <Grid container spacing ={10} direction = "column">
-            <Grid item>
-                <Header title = "Strategic Engagement Division"/>
-             </Grid>
-            <Grid item container spacing={10} justify="center">
-                <Grid item xs={6}>
-                  <Paper className={classes.paper}>xs=12</Paper>
-                </Grid>
-                <Grid item xs={6}>
-                  <Paper className={classes.paper}>xs=6</Paper>
-                </Grid>
-                <Grid item xs={6}>
-                  <Paper className={classes.paper}>xs=6</Paper>
-                </Grid>
-                <Grid item xs={6}>
-                  <Paper className={classes.paper}>xs=3</Paper>
-                </Grid>
-                <Grid item xs={6}>
-                  <Paper className={classes.paper}>xs=3</Paper>
-                </Grid>
-                <Grid item xs={6}>
-                  <Paper className={classes.paper}>xs=3</Paper>
-                </Grid>
-                <Grid item xs={6}>
-                  <Paper className={classes.paper}>xs=3</Paper>
-                </Grid>
-            </Grid>
-      </Grid>
-      </div>
-    );
-}
+                  {areas.map(function (area,i) {
+                    tmparr = [area, leaders[i], teams[i]]
+                    if(tmparr != null){
+      return (
+                      <Grid item xs={6} alignItems = 'center'>
+                        <Button variant="contained"  className = {classes.button}  >
+                            <Grid container item alignItems='center' direction = 'column'>
+                              <Grid item>
+                                <Typography variant ='h5'>{tmparr[0]}</Typography>
+                              </Grid>
+                              <Grid item>
+                              <Typography className={classes.subtitle} >
+                                  {tmparr[1]}
+                              </Typography>
+                              </Grid>
+
+                            </Grid>
+                          </Button>
+                      </Grid>
+                  )
+                }})
+
+  }
+              </Grid>
+          </Grid>
+
+          </div>
+      );
+  }
